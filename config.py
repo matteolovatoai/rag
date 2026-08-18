63311b0943db3ee6a063c06207e76887d55dc4ea
Matteo Lovato <matteolovatoai@proton.me>
2026-08-19 00:56:53 +0200

    aggiunto session id per la memoria con redis

--- config.py
+++ config.py
@@ -13,19 +13,22 @@ class Settings(BaseSettings):
     llm_model: str = "qwen2.5:3b"
     embeddings_model: str = "nomic-embed-text"
 
+    # Redis settings
+    redis_url: str = "redis://localhost:6379/0"
+
     chunk_size: int = 1000
     chunk_overlap: int = 200
     search_kwargs: int = 3
 
     # Settings for the RAG
-    rag_max_len_chat_history: int = 10
-    rag_contextualize_q_system_prompt: str = (
+    max_history: int = 10
+    contextualize_system_prompt: str = (
         "Data la cronologia della chat e l'ultima domanda dell'utente "
         "che potrebbe fare riferimento al contesto nella cronologia della chat, "
         "formula una domanda indipendente che possa essere compresa "
         "senza la cronologia. NON rispondere alla domanda, riformulala solo se necessario."
     )
-    rag_contextualize_q_prompt: str = (
+    contextualize_prompt: str = (
         "Sei un assistente utile ed esperto. Usa SOLO il seguente contesto per rispondere alla domanda. "
         "Se non conosci la risposta in base al contesto, dì semplicemente che non lo sai, non inventare nulla.\n\n"
         "Contesto: {context}"
